68a23cbfdce65012196e69d51a50a657fa5b8de2
olga voronets <voronets80@gmail.com>
2024-02-05 15:14:27 +0300

    Создано приложение materials. Описаны модели курса и урока

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'users'
+    'users',
+    'materials',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
